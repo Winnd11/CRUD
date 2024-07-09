3c3c83150172350cc54bb86a29b3b0072b8d6ea2
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-07-09 16:38:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require('express');
 const app = express();
 const port = 5500;
 const path = require('path');
+const sassMiddleware = require('node-sass-middleware');
+
+app.use(sassMiddleware( {
+    src: __dirname,
+    dest: __dirname,
+    debug: true,
+    outputStyle: 'compressed',
+}));
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.render('../public/ejs/index.ejs');
+    res.render(path.join(__dirname + '/public/ejs', 'index.ejs');
 });
 
 app.get('/crud', (req, res) => {
