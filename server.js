0d8dfde4463ab10375584f51898d0ca198d3cda4
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-07-09 22:11:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,30 @@ const app = express();
 const port = 5500;
 const path = require('path');
 const sassMiddleware = require('node-sass-middleware');
+const mysql = require('mysql);
+
+//mysql default port 3306                     
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: '', // your username
+    password: '1234', // your password
+    database: 'crud'
+});
+
+connection.connect();
+connection.query('create table crud', function (error, results, fields) {
+    if (error) {
+        throw error;
+    } 
+    console.log(results);
+});
 
 app.use(sassMiddleware( {
     src: __dirname,
     dest: __dirname,
     debug: true,
     outputStyle: 'compressed',
+    indentedSyntax: false, // set true, if you want '.sass' extension instead of '.scss'
 }));
 
 app.set('view engine', 'ejs');
@@ -22,6 +40,8 @@ app.get('/', (req, res) => {
 app.get('/crud', (req, res) => {
     res.send('recive');
 });
+
+connection.end();
 
 app.listen(port, () => {
     console.log(`running at http:/localhost:${port}`);
