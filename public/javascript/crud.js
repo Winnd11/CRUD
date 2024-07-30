33220a76f0e2f187a2218e573171b2829e2e3b0e
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-07-30 16:44:21 -0300

    Update crud.js

--- public/javascript/crud.js
+++ public/javascript/crud.js
@@ -6,21 +6,3 @@ const input = document.querySelector('input');
 buttonOpen.addEventListener('click', () => {
     dialog.showModal();
 });
-
-// body.addEventListener('click', (event) => {
-//     if (event.target === input) {
-//         dialog.close();
-//     }
-// });
-
-import { namesList } from '../../list';
-import { teste } from './list';
-
-// namesList.forEach(e => {
-//     const p = document.createElement('p');
-//     body.appendChild(p);
-//     p.innerText += e;
-//     console.log(na);
-// });
-
-console.log(teste());
